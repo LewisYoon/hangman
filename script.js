c81ee8c2ade8b9fe81523e3ab1f07e93a5855bc6
LewisYoon <trexhoon1212@gmail.com>
2026-06-29 18:11:35 +1000

    modifies to use arrow functions

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ const keyboard = document.querySelector("#keyboard");
 const message = document.querySelector("#message");
 const resetBtn = document.querySelector("#reset-btn");
 
-async function loadWords() {
+const loadWords = async () => {
   try {
-    const response = await fetch("assets/example-words.json");
+    const response = await fetch("./assets/example-words.json");
     words = await response.json();
     console.log(words);
   } catch (err) {
     console.error("Failed to load words:", err);
     throw err;
   }
-}
+};
 
-function displayKeyboard() {
+const displayKeyboard = () => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   for (let i = 0; i < alphabet.length; i++) {
     const char = alphabet[i];
@@ -36,48 +36,38 @@ function displayKeyboard() {
     btn.addEventListener("click", () => guess(char));
     keyboard.appendChild(btn);
   }
-}
+};
 
-function startGame() {
+const startGame = () => {
   guessedLetters = [];
-
   incorrectGuesses = 0;
-
   message.textContent = "";
-
   message.className = "hangman__message";
-
   const randomIndex = Math.floor(Math.random() * words.length);
-
   answer = words[randomIndex].toLowerCase();
-
   renderWords();
-}
+};
 
-function renderWords() {
+const renderWords = () => {
   let render = "";
-
   for (let i = 0; i < answer.length; i++) {
     const char = answer[i];
-
     if (guessedLetters.includes(char)) {
       render += char.toUpperCase() + " ";
     } else {
       render += "_ ";
     }
   }
-
   wordDisplay.textContent = render.trim();
-
   image.src = "assets/img/h-" + incorrectGuesses + ".jpg";
-}
+};
 
-async function play() {
+const play = async () => {
   await loadWords();
 
   displayKeyboard();
 
   startGame();
-}
+};
 
 play();
